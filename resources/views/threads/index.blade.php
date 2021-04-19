38512afc8e0aea56013fc1a1771f7b300fa82691
abdohadi <abdo.hady274@gmail.com>
2021-04-19 17:52:12 +0200

    Add authentication and test threads index and show

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Threads') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    @foreach ($threads as $thread)
+                        <article class="py-4">
+                            <h3 class="font-bold text-lg mb-2 text-blue-500">
+                                <a href="{{ route('threads.show', $thread) }}">{{ $thread->title }}</a>
+                            </h3>
+                            <div>{{ $thread->body }}</div>
+                        </article>
+                        <hr>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
